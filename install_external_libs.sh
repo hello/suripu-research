367f88c1222c69de52423f87d9f1f9b785d09f53
Benjamin Joseph <benjo@sayhello.com>
2016-03-09 10:12:55 -0800

    install external jars

---
+++ install_external_libs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mvn install:install-file -Dfile=./external_jars/deeplearning4j-core-0.4-rc3.9.jar -DgroupId=org.deeplearning4j -DartifactId=deeplearning4j-core -Dversion=0.4-rc3.9 -Dpackaging=jar
+mvn install:install-file -Dfile=./external_jars/lombok-1.16.7.jar -DgroupId=org.projectlombok -DartifactId=lombok -Dversion=1.16.7 -Dpackaging=jar
+
