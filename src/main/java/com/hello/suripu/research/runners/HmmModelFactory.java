dd0c80ef5dd73fe2e0507838ac99c93a9217a18e
Benjamin Joseph <benjo@sayhello.com>
2016-11-02 11:25:50 -0700

    added existing segmenting HMM for neural net to runner

--- src/main/java/com/hello/suripu/research/runners/HmmModelFactory.java
+++ src/main/java/com/hello/suripu/research/runners/HmmModelFactory.java
@@ -11,7 +11,74 @@ import com.hello.suripu.algorithm.hmm.PdfCompositeBuilder;
  * Created by benjo on 11/2/16.
  */
 public class HmmModelFactory {
+    public static HmmModel getExisting() {
 
+           /*
+     *  This takes the output of a neural network that outputs p(sleep), and a motion signal (on duration seconds) from the pill
+     *  and will output when it thinks you got in bed, fell asleep, woke up, and got out of bed.
+     *
+     *
+     *
+     *  There are two steps:
+     *  Step 1) Segment sleep with a hidden Markov model.
+     *
+     *                /--->{med(2)}----\
+     *               |                 v
+     *{med(1)} <--> {low(0)}        {high(3)} <-----> {med(4)}
+     *                ^                /
+     *                \-----{med(5)}---
+     *
+     *  The general idea is when you go from low to high, you will have a transition period on state 2
+     *  And when you go from high to low, you will have a transition period in state 5
+     *
+     *  When you're in state 2 you search for a sleep event
+     *  When you're in state 5 you search for a wake event
+     *
+     *  sleep event is max d/dt[p(sleep)] weighted by how close you are to p(sleep) = 0.5
+     *  so maybe weighed by p * (1 - p)
+     *
+     *  wake event is min d/dt[p(sleep)] * pill_svm_magnitude
+     *
+     *  Initial state is always state 0 or 1, and final state is always 0 or 1
+     *
+
+     */
+        
+        final  double MIN_HMM_PDF_EVAL = 1e-320;
+
+        final HmmPdfInterface[] obsModelsMain = {new BetaPdf(1.0,10.0,0),new BetaPdf(2.0,2.0,0),new BetaPdf(10.0,1.0,0)};
+
+        final HmmPdfInterface s0 = PdfCompositeBuilder.newBuilder().withPdf(obsModelsMain[0]).build();
+        final HmmPdfInterface s1 = PdfCompositeBuilder.newBuilder().withPdf(obsModelsMain[1]).build();
+        final HmmPdfInterface s2 = PdfCompositeBuilder.newBuilder().withPdf(obsModelsMain[1]).build();
+        final HmmPdfInterface s3 = PdfCompositeBuilder.newBuilder().withPdf(obsModelsMain[2]).build();
+        final HmmPdfInterface s4 = PdfCompositeBuilder.newBuilder().withPdf(obsModelsMain[1]).build();
+        final HmmPdfInterface s5 = PdfCompositeBuilder.newBuilder().withPdf(obsModelsMain[1]).build();
+
+
+        final HmmPdfInterface[] obsModels = {s0, s1, s2, s3, s4, s5};
+
+        final double[][] A = new double[obsModels.length][obsModels.length];
+        A[0][0] = 0.98; A[0][1] = 0.01; A[0][2] = 0.01;
+        A[1][0] = 0.02; A[1][1] = 0.98;
+        A[2][2] = 0.98; A[2][3] = 0.02;
+        A[3][3] = 0.98; A[3][4] = 0.01; A[3][5] = 0.01;
+        A[4][3] = 0.05; A[4][4] = 0.95;
+        A[5][0] = 0.02;                                                                 A[5][5] = 0.98;
+
+
+
+        final double[] pi = new double[obsModels.length];
+        pi[0] = 0.9;
+        pi[1] = 0.1;
+
+        //segment this shit
+        final HiddenMarkovModelInterface hmm = HiddenMarkovModelFactory.create(HiddenMarkovModelFactory.HmmType.LOGMATH, obsModels.length, A, pi, obsModels, 0);
+
+
+        return new HmmModel(hmm,new Integer[]{0,1},MIN_HMM_PDF_EVAL);
+
+    }
 
 
     public static HmmModel getFoobars() {
@@ -82,6 +149,10 @@ public class HmmModelFactory {
                 return Optional.of(getFoobars());
             }
 
+            case "existing":
+            {
+                return Optional.of(getExisting());
+            }
             default:
                 return Optional.absent();
         }
